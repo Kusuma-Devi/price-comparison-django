cb44dae444ea91cc2328d20fd93bd9f348bb7e1b
Kusuma-Devi <yanamadalakusuma@gmail.com>
2025-12-27 21:59:07 +0530

    Renamed main Django project to DigitalMarket

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SuperMarkIt.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DigitalMarket.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
